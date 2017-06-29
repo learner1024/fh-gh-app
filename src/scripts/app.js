da9fa32a5f9050c1a15e2cf8772fb71895d6c9d5
Sandeep Khandewale <learner1024@gmail.com>
2017-06-29 18:00:18 +0530

    updating gist now works. now need to modify search route config

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,4 +1,9 @@
 angular.module('fhooghle', ['ngRoute'])
+    // .config(['$httpProvider', function($httpProvider) {
+    //     $httpProvider.defaults.headers.patch = {
+    //         'Content-Type': 'application/json;charset=utf-8'
+    //     }
+    // }])
     .controller('searchController', ['$rootScope', '$scope', '$http', function($rootScope, $scope, $http){
         $scope.searchTrm = '';
         $scope.onSearchTrmChanged = function(){
@@ -28,15 +33,77 @@ angular.module('fhooghle', ['ngRoute'])
         })
         
     }])
-    .controller('gistsController', ['$scope', '$http', function($scope, $http){
+    .controller('gistsController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams){
+        $scope.userId = $routeParams.userId;
+
+        var gistId = sessionStorage.getItem('fhooghle_' + $scope.userId);
+
+        $scope.gistDescription = '';
+        $scope.gistContent = '';
+
+        if(gistId !== null){
+            $http.get('https://api.github.com/gists/' + gistId).then(
+                function(response){
+                    $scope.gistDescription = response.data.description;
+                    $scope.gistContent = response.data.files['gist.txt'].content;
+                },
+                function(response){
+                    console.log(response);
+                }
+            )
+        }
+
         $scope.postGist = function(){
-            console.log('creating gist...');
+            //if(gistId !== null){
+                //58533015cf37482aa2d73d81ee2e6b45
+                //first delete existing gist and then create new gist
+                //$http.delete('https://api.github.com/gists/' + gistId);
+                
+                // $http.patch('https://api.github.com/gists/' + gistId, {
+                //     "description": $scope.gistDescription,
+                //     "files": {
+                //         "gist.txt": {
+                //             "content": $scope.gistContent
+                //         }
+                //     }
+                // }).then(
+                //     function(response){
+                //         //success
+                //     },
+                //     function(response){
+                //         //failure
+                //         console.log(response);
+                //     }
+                // )
+            //}
+            //else {
+                $http.post('https://api.github.com/gists', {
+                    "description": $scope.gistDescription,
+                    "public": true,
+                    "files": {
+                        "gist.txt": {
+                            "content": $scope.gistContent
+                        }
+                    }
+                }).then(
+                    function(response){
+                        //success
+                        sessionStorage.setItem('fhooghle_' + $scope.userId, response.data.id);
+                    },
+                    function(response){
+                        //failure
+                        console.log(response);
+                    }
+                )
+            //}
+
+            
         }
         
     }])
     .config(function($routeProvider){
         $routeProvider
-            .when('/gists', {
+            .when('/gists/:userId', {
                 templateUrl: 'gist.html',
                 controller: 'gistsController'
             })
